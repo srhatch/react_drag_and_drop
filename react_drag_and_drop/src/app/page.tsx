1cad03efad2001ede886fbfddde5c2c563543bee
Sam Hatch <srhatch5@gmail.com>
2024-04-18 12:49:48 -0400

    feat(ImageForm): Add ImageForm component

--- react_drag_and_drop/src/app/page.tsx
+++ react_drag_and_drop/src/app/page.tsx
@@ -2,13 +2,15 @@
 import styles from "./page.module.scss";
 import { useState } from 'react';
 import ImageDisplay from './components/file_input/page';
+import { FormValues } from '../types/interfaces';
 // import FileInput
 
 export default function ImageForm() {
-  const [formValues, setFormValues] = useState({});
+  const [formValues, setFormValues] = useState<FormValues>({images: []});
+  console.log(formValues)
   return (
     <form>
-      <ImageDisplay/>
+      <ImageDisplay imageArray={formValues.images} setImageArray={setFormValues} />
     </form>
   );
 }
