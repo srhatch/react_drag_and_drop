c2ffcbbd72ab75a9dc21cd629d2d0475cbed3bea
Sam Hatch <srhatch5@gmail.com>
2024-04-18 17:05:40 -0400

    feats: Add 'getter' and 'setter' functions so child components can add images to state obj Add useEffect to prevent browser default behavior for dragover event Add ImageDisplay component

--- react_drag_and_drop/src/app/page.tsx
+++ react_drag_and_drop/src/app/page.tsx
@@ -1,16 +1,46 @@
 'use client';
 import styles from "./page.module.scss";
-import { useState } from 'react';
-import ImageDisplay from './components/file_input/page';
-import { FormValues } from '../types/interfaces';
-// import FileInput
+import { useState, useEffect } from 'react';
+import FileInput from './components/file_input/page';
+import ImageDisplay from './components/image_dispay/page';
+import { FormValues, UrlFile } from '../types/interfaces';
 
 export default function ImageForm() {
   const [formValues, setFormValues] = useState<FormValues>({images: []});
-  console.log(formValues)
+  //console.log(formValues)
+
+  useEffect(() => {
+    // Prevent the browser default "+" sign from showing next to the mouse when dragging a file across the page
+    document.addEventListener('dragover', (e) => {
+      e.preventDefault()
+    })
+  }, [])
+
+  function processAddImages(imageArray: File[]) {
+    const imageArrayUrls = imageArray.map(file => {
+      const browserUrl = URL.createObjectURL(file);
+      const imageFile: UrlFile = file;
+      imageFile.browserUrl = browserUrl;
+      return imageFile;
+    })
+    setFormValues((formValues: FormValues) => (
+      {...formValues, images: [...formValues.images, ...imageArrayUrls]}
+    ))
+  }
+
+  function removeImage(fileName: string) {
+    const filteredImages = formValues.images.filter(file => file.name !== fileName);
+    setFormValues((formValues: FormValues) => (
+      {...formValues, images: filteredImages}
+      ));
+  }
+
   return (
-    <form>
-      <ImageDisplay imageArray={formValues.images} setImageArray={setFormValues} />
-    </form>
+    <div className={styles.mainContainer}>
+      <form>
+        <FileInput imageArray={formValues.images} processAddImages={processAddImages} removeImage={removeImage} />
+      </form>
+      <ImageDisplay imageArray={formValues.images}/>
+    </div>
   );
 }
