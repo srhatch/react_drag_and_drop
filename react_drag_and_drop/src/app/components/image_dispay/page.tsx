2cf6a93d89a6000b14e375b463fc3678b52863d5
Sam Hatch <srhatch5@gmail.com>
2024-04-18 17:06:28 -0400

    init

---
+++ react_drag_and_drop/src/app/components/image_dispay/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+import styles from './ImageDisplay.module.scss';
+import { ImageDisplayProps } from '../../../types/interfaces';
+
+export default function ImageDisplay({ imageArray }: ImageDisplayProps) {
+
+    return (
+        <div className={styles.imageContainer}>
+            {
+                imageArray?.map(file => (
+                    <div className={styles.listingPicContainer} key={file.name}>
+                        <img
+                            className={styles.listingPics}
+                            src={file.browserUrl}
+                            alt=''
+                        />
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
