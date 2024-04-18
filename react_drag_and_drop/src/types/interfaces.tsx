427b2e6ac4ea336dc32a4c2659839740d71afff8
Sam Hatch <srhatch5@gmail.com>
2024-04-18 17:03:49 -0400

    feat(UrlFile, ImageDisplayProps): Add definitions Change FileInputProps name to ImageInputProps

--- react_drag_and_drop/src/types/interfaces.tsx
+++ react_drag_and_drop/src/types/interfaces.tsx
@@ -1,16 +1,24 @@
-import { SetStateAction } from "react";
-
 export interface ErrorObject {
     formatError?: boolean;
     numberExceededError?: boolean;
     sizeError?: boolean;
 }
 
-export interface FileInputProps {
+export interface ImageInputProps {
     imageArray: File[] | undefined;
-    setImageArray: React.Dispatch<SetStateAction<any>>
+    processAddImages: (imageArray: File[]) => void;
+    removeImage: (imageFile: string) => void;
+}
+
+export interface ImageDisplayProps {
+    imageArray: UrlFile[];
 }
 
 export interface FormValues {
     images: File[];
+}
+
+export interface UrlFile extends File {
+    // The objectUrl can be stored directly on the image File object
+    browserUrl?: string;
 }
