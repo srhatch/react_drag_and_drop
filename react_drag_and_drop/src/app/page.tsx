662b35aa03c3ead20527350011681be55b1bac7d
Sam Hatch <srhatch5@gmail.com>
2024-04-18 11:26:23 -0400

    style(scss import): Change stylesheet import to scss

--- react_drag_and_drop/src/app/page.tsx
+++ react_drag_and_drop/src/app/page.tsx
@@ -1,7 +1,14 @@
-import styles from "./page.module.css";
+'use client';
+import styles from "./page.module.scss";
+import { useState } from 'react';
+import ImageDisplay from './components/file_input/page';
+// import FileInput
 
-export default function FileInput() {
+export default function ImageForm() {
+  const [formValues, setFormValues] = useState({});
   return (
-    <div></div>
+    <form>
+      <ImageDisplay/>
+    </form>
   );
 }
