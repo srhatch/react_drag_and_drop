940846b2fa159fa42ec5aa81033f10f2d8ab8405
Sam Hatch <srhatch5@gmail.com>
2024-08-10 17:28:23 -0400

    (feat): Move error messaging logic to its own component

---
+++ react_drag_and_drop/src/app/components/error_message/page.tsx
@@ -0,0 +1,23 @@
+import { ErrorMessageProps } from "@/types/interfaces";
+
+export default function ErrorMessage({errorObject, className}: ErrorMessageProps) {
+    const errorFor = Object.values(errorObject).find(value => !!value);
+    const errorMsg = calcErrorMsg(errorFor || '');
+    return (
+        <div className={className}>{ errorMsg }</div>
+    )
+}
+
+// Helper functions
+function calcErrorMsg(errorFor: string) {
+    switch(errorFor) {
+        case 'formatError':
+            return 'File must be an image';
+        case 'sizeError':
+            return 'File must be less than 4MB';
+        case 'numberExceededError':
+            return 'Only 3 images can be uploaded';
+        default:
+            return '';
+    }
+}
