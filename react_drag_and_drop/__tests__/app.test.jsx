1b3bb9889107dc52b503bed49d4860829348425c
Sam Hatch <srhatch5@gmail.com>
2024-08-10 21:08:15 -0400

    (fix): Adjust tests to reflect new structure

--- react_drag_and_drop/__tests__/app.test.jsx
+++ react_drag_and_drop/__tests__/app.test.jsx
@@ -1,27 +1,24 @@
 import { it, expect } from '@jest/globals';
 import { render, screen, fireEvent } from '@testing-library/react';
-import ImageForm from '../src/app/page';
+import ParentComponent from '../src/app/page';
 import generateTestFile from './gen_test_files';
 
 it('adds an image to drop zone and image display onDrop', () => {
     const testJpegFile = generateTestFile('iAmAJpeg', 'image/jpeg', 3000000);
-    render(<ImageForm />);
+    render(<ParentComponent />);
     const dropZone = screen.getByTestId('dropZone');
     fireEvent.drop(dropZone, {
         dataTransfer: {
             files: [testJpegFile]
         }
     });
-    const fileDisplay = screen.getByText(testJpegFile.name);
-    expect(dropZone).toContainElement(fileDisplay);
-    const imageDisplay = screen.getByTestId('imageDisplay');
-    const inputImg = screen.getByRole('presentation');
-    expect(imageDisplay).toContainElement(inputImg);
-    expect(inputImg).toHaveAttribute('src', 'mockedURL');
-})
+    const imgElement = screen.getByRole('img');
+    expect(imgElement).toHaveAttribute('src', 'mockedURL');
+    expect(dropZone).toContainElement(imgElement);
+});
 it('adds an image to drop zone and image display onChange', () => {
     const testJpegFile = generateTestFile('iAmAJpeg', 'image/jpeg', 3000000);
-    render(<ImageForm />);
+    render(<ParentComponent />);
     const imageInput = screen.getByLabelText('Select from files');
     fireEvent.change(imageInput, {
         target: {
@@ -29,16 +26,13 @@ it('adds an image to drop zone and image display onChange', () => {
         }
     })
     const dropZone = screen.getByTestId('dropZone');
-    const fileDisplay = screen.getByText(testJpegFile.name);
-    expect(dropZone).toContainElement(fileDisplay);
-    const imageDisplay = screen.getByTestId('imageDisplay');
-    const inputImg = screen.getByRole('presentation');
-    expect(imageDisplay).toContainElement(inputImg);
-    expect(inputImg).toHaveAttribute('src', 'mockedURL');
-})
+    const imgElement = screen.getByRole('img');
+    expect(imgElement).toHaveAttribute('src', 'mockedURL');
+    expect(dropZone).toContainElement(imgElement);
+});
 it('displays an error message if file is too big', () => {
     const testJpegTooBig = generateTestFile('iAmAJpeg', 'image/jpeg', 4000001);
-    render(<ImageForm />);
+    render(<ParentComponent />);
     const imageInput = screen.getByLabelText('Select from files');
     fireEvent.change(imageInput, {
         target: {
@@ -47,10 +41,10 @@ it('displays an error message if file is too big', () => {
     })
     const errorMsg = screen.getByText('File must be less than 4MB');
     expect(errorMsg).toBeInTheDocument();
-})
+});
 it('displays an error message if file is not an image', () => {
     const notImageFile = generateTestFile('notAnImage', 'text/javascript');
-    render(<ImageForm />);
+    render(<ParentComponent />);
     const dropZone = screen.getByTestId('dropZone');
     fireEvent.drop(dropZone, {
         dataTransfer: {
@@ -59,13 +53,13 @@ it('displays an error message if file is not an image', () => {
     });
     const errorMsg = screen.getByText('File must be an image');
     expect(errorMsg).toBeInTheDocument();
-})
+});
 it('displays an error message if more than 3 files are uploaded', () => {
     const jpeg1 = generateTestFile('iAmAJpeg1', 'image/jpeg');
     const jpeg2 = generateTestFile('iAmAJpeg2', 'image/jpeg');
     const jpeg3 = generateTestFile('iAmAJpeg3', 'image/jpeg');
     const png = generateTestFile('iAmAPng', 'image/png');
-    render(<ImageForm />);
+    render(<ParentComponent />);
     const imageInput = screen.getByLabelText('Select from files');
     fireEvent.change(imageInput, {
         target: {
@@ -74,10 +68,10 @@ it('displays an error message if more than 3 files are uploaded', () => {
     })
     const errorMsg = screen.getByText('Only 3 images can be uploaded');
     expect(errorMsg).toBeInTheDocument();
-})
+});
 it('removes an image from dropzone and image display', () => {
     const jpegFile = generateTestFile('iAmAJpeg', 'image/jpeg');
-    render(<ImageForm />);
+    render(<ParentComponent />);
     const imageInput = screen.getByLabelText('Select from files');
     fireEvent.change(imageInput, {
         target: {
@@ -85,14 +79,13 @@ it('removes an image from dropzone and image display', () => {
         }
     })
     const deleteButton = screen.getByRole('button', {
-        name: '✕'
+        name: 'Remove image'
     });
     expect(deleteButton).toBeInTheDocument();
-    const dropZone = screen.getByTestId('dropZone');    
-    const fileDisplay = screen.getByText(jpegFile.name);
-    const imageDisplay = screen.getByTestId('imageDisplay');
-    const inputImg = screen.getByRole('presentation');
+    const dropZone = screen.getByTestId('dropZone');
+    const imgElement = screen.getByRole('img');
+    expect(dropZone).toContainElement(imgElement);
     fireEvent.click(deleteButton);
-    expect(dropZone).not.toContainElement(fileDisplay);
-    expect(imageDisplay).not.toContainElement(inputImg);
-})
+    expect(dropZone).not.toContainElement(imgElement);
+    expect(deleteButton).not.toBeInTheDocument();
+});
