a367f2ab552240a003300f805bb19c7beb1f7d82
srhatch <113044909+srhatch@users.noreply.github.com>
2024-08-12 11:32:11 -0400

    Delete react_drag_and_drop directory
    Remove duplicat directory

--- react_drag_and_drop/__tests__/run_test.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-npm run test utils.test.jsx
-npm run test file_utils.test.jsx
-npm run test app.test.jsx
--- react_drag_and_drop/jest.setup.ts
+++
@@ -1,6 +0,0 @@
-import '@testing-library/jest-dom';
-import { jest } from '@jest/globals';
-
-// Mock URL API methods so TypeError isn't thrown
-global.URL.createObjectURL = jest.fn(() => 'mockedURL');
-global.URL.revokeObjectURL = jest.fn(() => undefined);
